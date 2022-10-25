cf8f9581dec452cf70a3e75c68c3128b141fb76a
sujay2306 <sujayks007@gmail.com>
2022-10-25 19:33:56 +0530

    added strings

---
+++ Advanceish/strings.js
@@ -0,0 +1,9 @@
+var name = `sujay`
+var fullName = `${name} ks`
+
+console.log(fullName);
+
+console.log(fullName.charAt(0));
+console.log(fullName.endsWith("?"));
+console.log(fullName.includes("s"));
+console.log(fullName.toUpperCase());
