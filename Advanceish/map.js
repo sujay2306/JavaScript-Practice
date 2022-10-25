7fc6f6778724cf8801d318c4ce29150c927f2624
sujay2306 <sujayks007@gmail.com>
2022-10-25 20:09:48 +0530

    added destructure

--- Advanceish/map.js
+++ Advanceish/map.js
@@ -1,7 +1,25 @@
 var myMap = new Map()
 
 myMap.set(1,"uno")
-myMap.set(1,"uno")
-myMap.set(1,"uno")
-myMap.set(1,"uno")
-myMap.set(1,"uno")
+myMap.set(2,"thos")
+myMap.set(3,"thres")
+myMap.set(4,"quatro")
+
+
+// console.log(myMap);
+for(let keys of myMap.keys()){
+    console.log(`key is ${keys}`);
+}
+
+for(let values of myMap.values()){
+    console.log(`Value is ${values}`);
+}
+
+for(let [key,values] of myMap){
+    console.log(`key is ${key}, Value is ${values}`);
+}
+//delete
+myMap.delete(2)
+myMap.forEach((key, val ) => {
+    console.log(`${key}, ${val}`);
+})
