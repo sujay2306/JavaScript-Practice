fc3f9362198e0a6dab6885073bbe613819ba1d50
sujay2306 <sujayks007@gmail.com>
2022-10-26 13:43:32 +0530

    added async await promise

--- Advanceish/eventLoop.js
+++ Advanceish/eventLoop.js
@@ -1,18 +1,36 @@
 const uno = ()=>{
-    console.log("iam One");
+    return "iam One"
 }
 
-const dos = ()=>{
-    setTimeout(()=>{
-        console.log("ehoohoooo");
-    }, 3000)
-    console.log("iam Two");
+// const dos = ()=>{ //we can use async here got a op as promise undefined
+//     setTimeout(()=>{
+//        return "iam Two"
+//     }, 3000)
+   
+// }
+
+const dos = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            resolve ("iam Two") 
+        }, 3000)
+    })
 }
+
 
 const thres = ()=>{
-    console.log("iam thres");
+    return "iam thres"
 }
 
-uno()
-dos()
-thres()
+const callMe = async() =>{
+    let val1 = uno()
+
+    console.log(val1);
+    let val2 = await dos() //this tells wait for me dont leave so this will be waiting in que until it executes 
+    console.log(val2);
+
+    let val3 = thres()
+    console.log(val3);
+}
+
+callMe()
